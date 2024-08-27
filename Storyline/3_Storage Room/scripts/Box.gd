b4270260767ceab1712b2d472fedccbc336289d1
DarilRaf <darilmrafansyah@gmail.com>
2024-08-27 21:36:58 +0700

    polishing bagian 1

--- Storyline/3_Storage Room/scripts/Box.gd
+++ Storyline/3_Storage Room/scripts/Box.gd
@@ -13,6 +13,7 @@ signal start_task
 
 var camera: Camera2D
 var animation_played = false
+@onready var task_1 := GameStateManager.is_task_completed(1)
 
 func _ready():
 	Dialogic.signal_event.connect(_on_dialogic_signal)
@@ -29,30 +30,29 @@ func _on_dialogic_signal(argument:String):
 		animation_player.play("fade_to_black")
 
 
+#region Buba first encounter with Ucing
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "fade_to_black":
 		camera.position = Vector2(0,0)
 		await get_tree().create_timer(1.0).timeout
-		buba.set_global_position(Vector2(2210, 865))
+		buba.set_global_position(Vector2(1000, 805))
 		animation_player.play("fade_to_normal")
 	elif anim_name == "fade_to_normal":
 		color_rect.visible = false
-
-
+# ---
 func _on_body_entered(body):
-	if body.name == "MC" and !animation_played:
+	if body.name == "MC" and !animation_played and !GameStateManager.is_task_completed(1):
 		ucing.disable_movement()
 		box.play("dropped")
 		Dialogic.start("box_jatuh")
 		animation_played = true
-
-
+# ---
 func _on_animation_finished():
 	if Dialogic.current_timeline:
 		await Dialogic.timeline_ended
 	task_warning.set_visible(true)
-
-
+# ---
 func _on_button_pressed():
 	task_warning.set_visible(false)
 	start_task.emit()
+#endregion
--- Utility/GameStateManager.gd
+++ Utility/GameStateManager.gd
@@ -2,6 +2,22 @@ extends Node
 
 var ucing_pos_main := Vector2(800.0, 628.0)
 
+var tasks := {
+	1: false,
+	2: false,
+	3: false,
+	4: false,
+	5: false,
+	6: false,
+	7: false,
+	8: false
+}
+
+var free_roam_states := {
+	"Gudang": false,
+	"Kantin": false,
+	"Laundry": false
+}
 
 func update_pos_main(newPos: Vector2):
 	newPos.x -= 200
@@ -10,3 +26,29 @@ func update_pos_main(newPos: Vector2):
 
 func get_pos_main() -> Vector2:
 	return ucing_pos_main
+
+#region Task-related Methods
+func complete_task(task_number: int):
+	if task_number in tasks:
+		tasks[task_number] = true
+
+func reset_task(task_number: int):
+	if task_number in tasks:
+		tasks[task_number] = false
+
+func is_task_completed(task_number: int) -> bool:
+	return tasks.get(task_number, false)
+#endregion
+
+
+# Free roam state methods
+func set_free_roam_state(room: String, state: bool):
+	if room in free_roam_states:
+		free_roam_states[room] = state
+
+func get_free_roam_state(room: String) -> bool:
+	return free_roam_states.get(room, false)
+
+func add_new_room(room: String):
+	if room not in free_roam_states:
+		free_roam_states[room] = false
